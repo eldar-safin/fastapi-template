56defbc841d50b39a39c4e0190218bd3c44b156b
Eldar Safin <github@eldar-s.online>
2023-07-14 08:48:56 +0300

    Add app

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get('/')
+async def index():
+  return {'status': 'OK'}
+
+
+if __name__ == '__main__':
+  uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
